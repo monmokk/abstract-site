61aa657d5bc1fa9eba5e3a287ac888a7f6fa7e7d
monmokk <myeongjikang910814@gmail.com>
2022-06-03 17:40:00 +0900

    jest, prettier

--- src/index.js
+++ src/index.js
@@ -1,14 +1,87 @@
-class Site {}
+class Site {
+    constructor() {
+        this.boards = [];
+    }
 
-class Board {}
+    addBoard(board) {
+        if (this.boards.find((boardInfo) => boardInfo.name === board.name)) {
+            throw Error();
+        }
+        board.myBoard = true;
+        this.boards.push(board);
+    }
 
-class Article {}
+    findBoardByName(name) {
+        // eslint-disable-next-line no-return-assign
+        return this.boards.find((board) => (board.name = name));
+    }
+}
 
-class Comment {}
+class Board {
+    constructor(name) {
+        if (name === null || name === '') {
+            throw Error();
+        }
+        this.name = name;
+        this.myBoard = false;
+        this.articles = [];
+    }
 
-module.exports = {
-    Site,
-    Board,
-    Article,
-    Comment,
-};
+    publish(article) {
+        if (this.myBoard === false) {
+            throw Error();
+        }
+        article.id = `${this.name}-${Math.random()}`;
+        article.createdDate = new Date().toISOString();
+        this.articles.push(article);
+    }
+
+    getAllArticles() {
+        return this.articles;
+    }
+}
+
+class Article {
+    constructor(article) {
+        const { subject, content, author } = article;
+        if (
+            subject === null ||
+            subject === '' ||
+            content === null ||
+            content === '' ||
+            author === null ||
+            author === ''
+        ) {
+            throw Error();
+        }
+        this.subject = subject;
+        this.content = content;
+        this.author = author;
+        this.comments = [];
+    }
+
+    reply(comment) {
+        if (!this.id) {
+            throw Error();
+        }
+        comment.createdDate = new Date().toISOString();
+        this.comments.push(comment);
+    }
+
+    getAllComments() {
+        return this.comments;
+    }
+}
+
+class Comment {
+    constructor(comment) {
+        const { content, author } = comment;
+        if (content === null || content === '' || author === null || author === '') {
+            throw Error();
+        }
+        this.content = content;
+        this.author = author;
+    }
+}
+
+module.exports = { Site, Board, Article, Comment };
